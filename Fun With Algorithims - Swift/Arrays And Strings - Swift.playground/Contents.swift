38778eacf98028c4ac4b227e753b8af1da716e27
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2020-10-21 22:02:40 +0100

    Added Solution for Contains Duplicates
    Added func containsDups() that checks if a String contains duplicates

--- Fun With Algorithims - Swift/Arrays And Strings - Swift.playground/Contents.swift
+++ Fun With Algorithims - Swift/Arrays And Strings - Swift.playground/Contents.swift
@@ -16,3 +16,32 @@ func printMatrix(_ mat: [[Any]]) {
     print(output)
 }
 
+var str = "Hello, playground"
+var str1 = "abcdeA"
+var str2 = "bcaAde"
+var str3 = "abc"
+var arr = Array(str) + [" ", " ", " ", " "]
+var mat = [[0, 2, 3, 4],
+           [5, 6, 7, 8],
+           [9, 0, 11, 12],
+           [13, 14, 15, 16]]
+
+// -- Solutions --
+
+// 1a. Time: O(n2), Space: O(1)
+func containsDups(s: String?) -> Bool {
+    guard let string = s else { return false }
+    if string.isEmpty { return false }
+    
+    for i in 0 ..< string.count-1 {
+        for j in i+1 ..< string.count {
+            let firstIndex = string.index(string.startIndex, offsetBy: i)
+            let secondIndex = string.index(string.startIndex, offsetBy: j)
+            if string[firstIndex] == string[secondIndex] {
+                return true
+            }
+        }
+    }
+    
+    return false
+}
