c8f9705bd585a61fe121c3bf84d1356c745a908a
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2020-11-12 22:12:25 +0100

    Added Solution to Remove Duplicates Problem
    Added removeDups() function that removes duplicates in a LinkedList using 2 loops

--- Fun With Algorithims - Swift/Linked Lists - Swift.playground/Contents.swift
+++ Fun With Algorithims - Swift/Linked Lists - Swift.playground/Contents.swift
@@ -24,3 +24,26 @@ func calcLength(_ node: ListNode?) -> Int {
     return length
 }
 
+// Problem 1. Remove Duplicates
+
+// Solution 1a: 2 loops. Time: O(n^2), Space: O(1)
+func removeDups(_ head: ListNode) {
+    var current: ListNode! = head
+    
+    while current.next != nil {
+        var previous: ListNode! = current
+        var runner: ListNode! = current.next
+        while runner != nil {
+            if current.val == runner.val {
+                previous.next = runner.next
+            } else {
+                previous = runner
+            }
+            
+            runner = runner.next
+        }
+        
+        current = current.next
+    }
+}
+
