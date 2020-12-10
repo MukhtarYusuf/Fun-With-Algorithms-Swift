155fb4c94774f5f40c40450cb0eecb9f473c8856
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2020-12-10 22:12:28 +0100

    Added Stacks and Queues Project
    Added new MacOS Command Line Project to solve stacks and queues problems

---
+++ Fun With Algorithims - Swift/Stacks and Queues/Stacks and Queues/StacksAndQueuesDemo.swift
@@ -0,0 +1,14 @@
+//
+//  StacksAndQueuesDemo.swift
+//  Stacks and Queues
+//
+//  Created by Mukhtar Yusuf on 11/5/20.
+//  Copyright © 2020 Mukhtar Yusuf. All rights reserved.
+//
+
+import Foundation
+
+class StacksAndQueuesDemo {
+    static func main() {
+    }
+}
---
+++ Fun With Algorithims - Swift/Stacks and Queues/Stacks and Queues/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Stacks and Queues
+//
+//  Created by Mukhtar Yusuf on 11/2/20.
+//  Copyright © 2020 Mukhtar Yusuf. All rights reserved.
+//
+
+import Foundation
+
+StacksAndQueuesDemo.main()
