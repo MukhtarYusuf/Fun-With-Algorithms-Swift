4388d26d4c1b2134c7ab0af5e04cc0b1b8f4775b
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2020-12-29 21:52:26 +0100

    Added ThreeStack1 Class
    Added ThreeStack1 class, optimized solution to problem "Three in One" that implements three stacks using a single array

---
+++ Fun With Algorithims - Swift/Stacks and Queues/Stacks and Queues/ThreeStack1.swift
@@ -0,0 +1,38 @@
+//
+//  ThreeStack1.swift
+//  Stacks and Queues
+//
+//  Created by Mukhtar Yusuf on 11/3/20.
+//  Copyright © 2020 Mukhtar Yusuf. All rights reserved.
+//
+
+import Foundation
+
+// Problem 1: Three in One
+
+// Solution 1b: Dynamic Arrangement of Stack Elements
+
+class ThreeStack1 {
+    private var arr: [StackNode]
+    private var tops: [Int]
+    private var STACK_SIZE: Int
+    private var globalIndex: Int
+}
+
+/*
+ for push
+    set lastTop for node
+    update top for stack
+    if globalIndex != -1
+        update globalIndex + 1
+    else
+        update globalIndex = nextIndex
+ 
+ for pop
+    if popIndex < globalIndex
+        set nextIndex = globalIndex
+        set globalIndex = popIndex
+    else
+        arr[popIndex].nextIndex = arr[globalIndex].nextIndex
+        arr[globalIndex].nextIndex = popIndex
+ */
