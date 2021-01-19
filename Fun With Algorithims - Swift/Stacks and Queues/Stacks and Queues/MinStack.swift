3c32e874e979f73927b8c91c501f515eab74dca1
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2021-01-18 23:38:36 -0500

    Added MinStack class
    Added MinStack class for solution to "Stack Min" problem

---
+++ Fun With Algorithims - Swift/Stacks and Queues/Stacks and Queues/MinStack.swift
@@ -0,0 +1,27 @@
+//
+//  MinStack.swift
+//  Stacks and Queues
+//
+//  Created by Mukhtar Yusuf on 11/3/20.
+//  Copyright © 2020 Mukhtar Yusuf. All rights reserved.
+//
+
+import Foundation
+
+// Problem 2: Stack Min
+
+// Solution: Custom StackNode Approach
+
+class MinStack {
+}
+
+
+// min = 3,
+// push(2), min = 2
+// pop() -> 2, min = 3,
+// Needed: 5 -> 3 -> 2
+
+// bottom -> top
+// 10, min: 10 -> 11, min: 10 -> 9, min: 9 -> 12, min: 9
+
+
