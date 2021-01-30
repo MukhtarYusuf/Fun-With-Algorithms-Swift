e1508a47051a72b000196937faa145f1c059d702
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2021-01-29 21:11:23 -0500

    Added Stack Class Node Based Implementation

---
+++ Fun With Algorithims - Swift/Stacks and Queues/Stacks and Queues/Stack.swift
@@ -0,0 +1,26 @@
+//
+//  Stack.swift
+//  Stacks and Queues
+//
+//  Created by Mukhtar Yusuf on 11/2/20.
+//  Copyright © 2020 Mukhtar Yusuf. All rights reserved.
+//
+
+import Foundation
+
+// This is a Stack Node Based Implementation
+
+class Stack<T> {
+    private class StackNode<T> {
+        var val: T
+        var next: StackNode?
+        
+        init(val: T) {
+            self.val = val
+        }
+    }
+    
+    private var top: StackNode<T>?
+    
+    
+}
