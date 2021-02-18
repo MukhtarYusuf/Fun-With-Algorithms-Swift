a225eb67efa9f47ad5cc798032158e07a80a918b
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2021-02-17 20:57:48 -0500

    Create Stack1.swift
    Create class definition for array based stack with backing array

---
+++ Fun With Algorithims - Swift/Stacks and Queues/Stacks and Queues/Stack1.swift
@@ -0,0 +1,16 @@
+//
+//  Stack1.swift
+//  Stacks and Queues
+//
+//  Created by Mukhtar Yusuf on 11/3/20.
+//  Copyright © 2020 Mukhtar Yusuf. All rights reserved.
+//
+
+import Foundation
+
+// Array Based Implementation
+
+class Stack1<T> {
+    private var arr: [T]
+
+}
