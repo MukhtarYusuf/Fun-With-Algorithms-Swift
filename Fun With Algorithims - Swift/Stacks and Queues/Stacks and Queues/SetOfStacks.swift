272af158145d0336db003224386c78f234eee7e0
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2021-03-12 20:20:16 -0500

    Create SetOfStacks.swift

---
+++ Fun With Algorithims - Swift/Stacks and Queues/Stacks and Queues/SetOfStacks.swift
@@ -0,0 +1,17 @@
+//
+//  SetOfStacks.swift
+//  Stacks and Queues
+//
+//  Created by Mukhtar Yusuf on 11/4/20.
+//  Copyright © 2020 Mukhtar Yusuf. All rights reserved.
+//
+
+import Foundation
+
+// Problem 3: Stack of Plates. Follow up: popAtIndex()
+
+// Solution: Array of Stacks With Implicit Capacity
+
+class SetOfStacks<T> {
+
+}
