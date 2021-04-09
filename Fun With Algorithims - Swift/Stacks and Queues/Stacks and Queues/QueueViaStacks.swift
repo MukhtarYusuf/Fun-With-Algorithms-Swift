79d5eb671de16f80247ef8d28dea4887a13caeae
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2021-04-08 23:27:33 -0400

    Create QueueViaStacks.swift
    Create QueueViaStacks to implement a Queue using two stacks

---
+++ Fun With Algorithims - Swift/Stacks and Queues/Stacks and Queues/QueueViaStacks.swift
@@ -0,0 +1,15 @@
+//  QueueViaStacks.swift
+//  Stacks and Queues
+//
+//  Created by Mukhtar Yusuf on 11/4/20.
+//  Copyright © 2020 Mukhtar Yusuf. All rights reserved.
+//
+
+import Foundation
+
+// Problem 4: Queue via Stacks
+
+// Solution: 2 Stacks Approach
+
+class QueueViaStacks<T> {
+}
