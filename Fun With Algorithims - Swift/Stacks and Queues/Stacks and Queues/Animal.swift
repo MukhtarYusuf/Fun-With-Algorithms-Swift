e229692790a2ffe6624e9236ea670e16763233b9
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2023-04-28 02:07:55 -0400

    Create Animal.swift
    Add Animal class

---
+++ Fun With Algorithims - Swift/Stacks and Queues/Stacks and Queues/Animal.swift
@@ -0,0 +1,14 @@
+//
+//  Animal.swift
+//  Stacks and Queues
+//
+//  Created by Mukhtar Yusuf on 11/5/20.
+//  Copyright © 2020 Mukhtar Yusuf. All rights reserved.
+//
+
+import Foundation
+
+class Animal {
+    var timeStamp = 0
+    var name = ""
+}
