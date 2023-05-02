d40d1200c11a1110a386fb646a4f64d82ae0c005
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2023-05-02 19:12:21 -0400

    Create Cat.swift
    Add Cat class

---
+++ Fun With Algorithims - Swift/Stacks and Queues/Stacks and Queues/Cat.swift
@@ -0,0 +1,18 @@
+//
+//  Cat.swift
+//  Stacks and Queues
+//
+//  Created by Mukhtar Yusuf on 11/5/20.
+//  Copyright © 2020 Mukhtar Yusuf. All rights reserved.
+//
+
+import Foundation
+
+class Cat: Animal {
+    init(timeStamp: Int) {
+        super.init()
+        self.name = "Cat"
+        self.timeStamp = timeStamp
+    }
+
+}
