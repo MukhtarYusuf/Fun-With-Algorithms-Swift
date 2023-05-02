cf4967aec89adb7c67aacc43fbc5f012ecc49905
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2023-05-01 21:04:30 -0400

    Create Dog.swift
    Add Dog class

---
+++ Fun With Algorithims - Swift/Stacks and Queues/Stacks and Queues/Dog.swift
@@ -0,0 +1,18 @@
+//
+//  Dog.swift
+//  Stacks and Queues
+//
+//  Created by Mukhtar Yusuf on 11/5/20.
+//  Copyright © 2020 Mukhtar Yusuf. All rights reserved.
+//
+
+import Foundation
+
+class Dog: Animal {
+    init(timeStamp: Int) {
+        super.init()
+        self.name = "Dog"
+        self.timeStamp = timeStamp
+    }
+
+}
